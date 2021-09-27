3accab3e6c7e27e8e60b75e8e1740008d3c9f90a
quddufrkawk <quddufrkawk@naver.com>
2021-09-27 17:08:59 +0900

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
@@ -27,22 +28,33 @@ const foodILike = [
   }
 ];
 
-function rendorFood(dish){
- return <Food key={dish.id} name={dish.name} picture={dish.image} />
-}
 
-function Food({name, picture}){
+function Food({name, picture, rating}){
   return (
   <div>
     <h2>L Like {name}</h2>
+    <h4>{rating}/5.0</h4>
     <img src={picture} alt={name} />
   </div>
   );
 }
 
+Food.propTypes ={
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
 function App() {
   return  <div>
-      {foodILike.map(rendorFood)}
+      {foodILike.map(dish =>(
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
 }
 
